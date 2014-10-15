89ee78678f2dc1a1715eabb212ffc3fb7d9c394c
root <root@pikachu.anam.com>
2014-10-15 16:55:10 +0100

    The cmake script expects this file to be under /unix not /posix

---
+++ src/greeter/unix/greeter.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   Greeter.cpp
+ * Author: tony.lowry
+ * 
+ * Created on 06 March 2014, 17:26
+ */
+
+#include "../greeter.h"
+
+namespace greet {
+
+    Greeter::Greeter() {
+    }
+
+    Greeter::Greeter(const Greeter& orig) {
+    }
+
+	std::string Greeter::Greet() {
+		std::string resp = "Greetings from the Posix greeting implementation";
+		return resp;
+    }
+	
+    Greeter::~Greeter() {
+    }
+}
