4d389a58df24ff82215b8df78427ca40a64aad77
Tony Lowry <tonylowryie@gmail.com>
2014-10-15 17:45:01 +0100

    Small change to wait for user input

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,9 @@ int main(int argc, char** argv) {
 
 	// Create a platform specific greeter and print it's greeting
 	greet::Greeter myGreeter = greet::Greeter();
-	std::cout << myGreeter.Greet();
+	std::cout << myGreeter.Greet() << std::endl;
+	std::cout << "Press any key to quit" << std::endl;
+	std::cin.get();
     return 0;
 }
 
